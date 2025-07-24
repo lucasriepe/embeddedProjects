72d0ce0ecaf8eef8ae832c0294b6a966e151619e
Lucas <riepelucas@gmail.com>
2025-07-24 17:53:47 +0200

    chore: update wifi config and gitignore for weather station
    - Add wifi credentials to wifi_config.h
    - Create .env.example with configuration template
    - Update .gitignore to exclude wifi_config.h and clean up entries

--- Espressif IDE/weatherStation/mainStation/main/wifi_config.h
+++ Espressif IDE/weatherStation/mainStation/main/wifi_config.h
@@ -3,6 +3,9 @@
 
 // Automatically generated from .env file
 // DO NOT EDIT MANUALLY!
+
+#define WIFI_SSID "FRITZ!Box 7590 NM"
+#define WIFI_PASS "95065973249267578128"
 #define TIMEZONE_STRING "CET-1CEST,M3.5.0,M10.5.0/3"
 
 #endif // WIFI_CONFIG_H
