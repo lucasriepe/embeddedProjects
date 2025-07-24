5ab7fc2db489987bddf024daba73815f674c219e
Lucas <riepelucas@gmail.com>
2025-07-24 16:58:53 +0200

    feat(httpDHT22): add HTTP server handler for hello endpoint
    Implement basic HTTP server with a hello world endpoint
    Add WiFi initialization before starting server

--- Espressif IDE/weatherStation/httpDHT22/main/main.c
+++ Espressif IDE/weatherStation/httpDHT22/main/main.c
@@ -6,10 +6,12 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 
+//*HTTP header
+#include <esp_http_server.h>
+
+//*WiFi setup
 #define EXAMPLE_WIFI_SSID CONFIG_WIFI_SSID
 #define EXAMPLE_WIFI_PASS CONFIG_WIFI_PASSWORD
-
-//*HTTP header
 
 void wifi_init(void)
 {
@@ -46,7 +48,32 @@ ESP_ERROR_CHECK(esp_wifi_connect());
 //WIFI-END-------------------------------------------------------------------------------------
 }
 
+static esp_err_t hello_get_handler(httpd_req_t *req) {
+    const char resp[] = "Hello, World!";
+    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+static httpd_uri_t hello = {
+    .uri       = "/hello",
+    .method    = HTTP_GET,
+    .handler   = hello_get_handler,
+    .user_ctx  = NULL
+};
+
+void start_webserver(void) {
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    httpd_handle_t server = NULL;
+    if (httpd_start(&server, &config) == ESP_OK) {
+        httpd_register_uri_handler(server, &hello);
+    }
+}
+
 void app_main(void)
 {
+    //?WiFi start
     wifi_init();
+
+    //?HTTP server start
+    //start_webserver();
 }
